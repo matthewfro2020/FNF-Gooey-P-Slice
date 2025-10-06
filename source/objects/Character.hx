4cbb9bb6371fb4df369aec046a69e54830159337
matthewfro181818 <158477647+matthewfro181818@users.noreply.github.com>
2025-10-06 08:53:50 -0400

    Update Character.hx

--- source/objects/Character.hx
+++ source/objects/Character.hx
@@ -122,6 +122,250 @@ class Character extends FlxPerspectiveSprite
 		this.isPlayer = isPlayer;
 		changeCharacter(character);
 
+		switch (curCharacter)
+		{
+			//case 'your character name in case you want to hardcode them instead':
+
+			default:
+				var characterPath:String = 'characters/' + curCharacter + '.json';
+
+
+				#if MODS_ALLOWED
+				var path:String = Paths.modFolders(characterPath);
+				if (!FileSystem.exists(path)) {
+					path = Paths.getPreloadPath(characterPath);
+				}
+
+				if (!FileSystem.exists(path))
+				#else
+				var path:String = Paths.getPreloadPath(characterPath);
+				if (!Assets.exists(path))
+				#end
+				{
+					path = Paths.getPreloadPath('characters/' + DEFAULT_CHARACTER + '.json'); //If a character couldn't be found, change him to BF just to prevent a crash
+				}
+
+				#if MODS_ALLOWED
+				var rawJson = File.getContent(path);
+				#else
+				var rawJson = Assets.getText(path);
+				#end
+
+				var json:CharacterFile = cast Json.parse(rawJson);
+
+				if(json.characters == null || json.characters.length <= 1)
+				{
+					var spriteType = "sparrow";
+					//sparrow
+					//packer
+					//texture
+					#if MODS_ALLOWED
+					var modTxtToFind:String = Paths.modsTxt(json.image);
+					var txtToFind:String = Paths.getPath('images/' + json.image + '.txt', TEXT);
+					
+					//var modTextureToFind:String = Paths.modFolders("images/"+json.image);
+					//var textureToFind:String = Paths.getPath('images/' + json.image, new AssetType();
+					
+					if (FileSystem.exists(modTxtToFind) || FileSystem.exists(txtToFind) || Assets.exists(txtToFind))
+					#else
+					if (Assets.exists(Paths.getPath('images/' + json.image + '.txt', TEXT)))
+					#end
+					{
+						spriteType = "packer";
+					}
+						
+					#if MODS_ALLOWED
+					var modAnimToFind:String = Paths.modFolders('images/' + json.image + '/Animation.json');
+					var animToFind:String = Paths.getPath('images/' + json.image + '/Animation.json', TEXT);
+						
+					//var modTextureToFind:String = Paths.modFolders("images/"+json.image);
+					//var textureToFind:String = Paths.getPath('images/' + json.image, new AssetType();
+						
+					if (FileSystem.exists(modAnimToFind) || FileSystem.exists(animToFind) || Assets.exists(animToFind))
+					#else
+					if (Assets.exists(Paths.getPath('images/' + json.image + '/Animation.json', TEXT)))
+					#end
+					{
+						spriteType = "texture";
+					}
+
+					switch (spriteType){
+							
+						case "packer":
+							frames = Paths.getPackerAtlas(json.image);
+							
+						case "sparrow":
+								frames = Paths.getSparrowAtlas(json.image);
+							
+						case "texture":
+							frames = AtlasFrameMaker.construct(json.image);
+					}
+					imageFile = json.image;
+	
+					if(json.scale != 1) {
+						jsonScale = json.scale;
+						setGraphicSize(Std.int(width * jsonScale));
+						updateHitbox();
+					}
+	
+					if (json.maxhealth >= 0)
+						maxhealth = json.maxhealth;
+					else
+						maxhealth = 2;
+					
+					if (json.minhealth <= json.maxhealth)
+						minhealth = json.minhealth;
+					else
+						minhealth = 0;
+					
+					positionArray = json.position;
+					cameraPosition = json.camera_position;
+
+					if (json.rating_position != null)
+						ratingPosition = json.rating_position;
+
+					if (json.rating_position == null)
+						ratingPosition = [0, 0];
+
+					healthIcon = json.healthicon;
+					singDuration = json.sing_duration;
+					flipX = !!json.flip_x;
+					if(json.no_antialiasing) {
+						antialiasing = false;
+						noAntialiasing = true;
+					}
+	
+					if(json.healthbar_colors != null && json.healthbar_colors.length > 2)
+						healthColorArray = json.healthbar_colors;
+	
+					antialiasing = !noAntialiasing;
+					if(!ClientPrefs.data.globalAntialiasing) antialiasing = false;
+	
+					animationsArray = json.animations;
+					if(animationsArray != null && animationsArray.length > 0) {
+						for (anim in animationsArray) {
+							var animAnim:String = '' + anim.anim;
+							var animName:String = '' + anim.name;
+							var animFps:Int = anim.fps;
+							var animLoop:Bool = !!anim.loop; //Bruh
+							var animIndices:Array<Int> = anim.indices;
+							if(animIndices != null && animIndices.length > 0) {
+								animation.addByIndices(animAnim, animName, animIndices, "", animFps, animLoop);
+							} else {
+								animation.addByPrefix(animAnim, animName, animFps, animLoop);
+							}
+			
+							if(anim.offsets != null && anim.offsets.length > 1) {
+								addOffset(anim.anim, anim.offsets[0], anim.offsets[1]);
+							}
+						}
+					} else {
+						quickAnimAdd('idle', 'BF idle dance');
+					}
+					cameraPosition = json.camera_position;
+					//trace('Loaded file to character ' + curCharacter);
+				}
+				else
+					loadNamedConfiguration(json);
+		}
+		originalFlipX = flipX;
+
+		if(animOffsets.exists('singLEFTmiss') || animOffsets.exists('singDOWNmiss') || animOffsets.exists('singUPmiss') || animOffsets.exists('singRIGHTmiss')) hasMissAnimations = true;
+		recalculateDanceIdle();
+		dance();
+
+		if(curCharacter != '' && otherCharacters == null && animation.curAnim != null)
+			{
+				updateHitbox();
+	
+				if (isPlayer)
+				{
+					flipX = !flipX;
+		
+					/*// Doesn't flip for BF, since his are already in the right place???
+					if (!curCharacter.startsWith('bf'))
+					{
+						// var animArray
+						if(animation.getByName('singLEFT') != null && animation.getByName('singRIGHT') != null)
+						{
+							var oldRight = animation.getByName('singRIGHT').frames;
+							animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
+							animation.getByName('singLEFT').frames = oldRight;
+						}
+		
+						// IF THEY HAVE MISS ANIMATIONS??
+						if (animation.getByName('singLEFTmiss') != null && animation.getByName('singRIGHTmiss') != null)
+						{
+							var oldMiss = animation.getByName('singRIGHTmiss').frames;
+							animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
+							animation.getByName('singLEFTmiss').frames = oldMiss;
+						}
+					}*/
+					if(!isPlayer)
+					{
+						// Doesn't flip for BF, since his are already in the right place???
+						if(opponentflip_X)
+						{
+							var oldOffRight = animOffsets.get("singRIGHT");
+							var oldOffLeft = animOffsets.get("singLEFT");
+			
+							// var animArray
+							var oldRight = animation.getByName('singRIGHT').frames;
+							animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
+							animation.getByName('singLEFT').frames = oldRight;
+			
+							animOffsets.set("singRIGHT", oldOffLeft);
+							animOffsets.set("singLEFT", oldOffRight);
+			
+							// IF THEY HAVE MISS ANIMATIONS??
+							if (animation.getByName('singRIGHTmiss') != null)
+							{
+								var oldOffRightMiss = animOffsets.get("singRIGHTmiss");
+								var oldOffLeftMiss = animOffsets.get("singLEFTmiss");
+			
+								var oldMiss = animation.getByName('singRIGHTmiss').frames;
+								animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
+								animation.getByName('singLEFTmiss').frames = oldMiss;
+			
+								animOffsets.set("singRIGHTmiss", oldOffLeftMiss);
+								animOffsets.set("singLEFTmiss", oldOffRightMiss);
+							}
+						}
+					}
+					if(isPlayer)
+					{
+						// Doesn't flip for BF, since his are already in the right place???
+						if(bfflip_X)
+						{
+							var oldOffRight = animOffsets.get("singRIGHT");
+							var oldOffLeft = animOffsets.get("singLEFT");
+			
+							// var animArray
+							var oldRight = animation.getByName('singRIGHT').frames;
+							animation.getByName('singRIGHT').frames = animation.getByName('singLEFT').frames;
+							animation.getByName('singLEFT').frames = oldRight;
+			
+							animOffsets.set("singRIGHT", oldOffLeft);
+							animOffsets.set("singLEFT", oldOffRight);
+			
+							// IF THEY HAVE MISS ANIMATIONS??
+							if (animation.getByName('singRIGHTmiss') != null)
+							{
+								var oldOffRightMiss = animOffsets.get("singRIGHTmiss");
+								var oldOffLeftMiss = animOffsets.get("singLEFTmiss");
+			
+								var oldMiss = animation.getByName('singRIGHTmiss').frames;
+								animation.getByName('singRIGHTmiss').frames = animation.getByName('singLEFTmiss').frames;
+								animation.getByName('singLEFTmiss').frames = oldMiss;
+			
+								animOffsets.set("singRIGHTmiss", oldOffLeftMiss);
+								animOffsets.set("singLEFTmiss", oldOffRightMiss);
+							}
+						}
+					}
+				}
+			}
+
 		switch(curCharacter)
 		{
 			case 'pico-blazin', 'darnell-blazin':
